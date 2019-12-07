505cd775d4fd69c33ae96bf04cee44fbcbf50b32
jan grant <jan.grant@oracle.com>
2019-12-07 09:40:03 +0000

    Day 7
    Tying the knot - including along an arbitrary sequence of
    computations. Lazy evaluation is great.

--- test/Spec.hs
+++ test/Spec.hs
@@ -11,6 +11,7 @@ import qualified Day4
 import qualified Intcode
 import qualified Day5
 import qualified Day6
+import qualified Day7
 
 
 main :: IO ()
@@ -121,9 +122,30 @@ main =
         Day6.orbitalDistance ot "L" "E" `shouldBe` 3
         Day6.orbitalDistance ot "COM" "COM" `shouldBe` 0
 
+    describe "Day7" $ do
+      it "runs example 1" $ do
+        Day7.day7 ["3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"] `shouldBe` 43210
+
+      it "runs example 2" $ do
+        Day7.day7 ["3,23,3,24,1002,24,10,24,1002,23,-1,23,\
+                   \101,5,23,23,1,24,23,23,4,23,99,0,0"] `shouldBe` 54321
+
+      it "runs example3" $ do
+        Day7.day7 ["3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,\
+                   \1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"] `shouldBe` 65210
+        
+    describe "Day7b" $ do
+      it "does example 1" $ do
+        Day7.day7b ["3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,\
+                    \27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"] `shouldBe` 139629729
+
+      it "does example 2" $ do
+        Day7.day7b ["3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,\
+                    \-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,\
+                    \53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"] `shouldBe` 18216
+
+
 {-
-    describe "Day7" $ do it "works" $ do Day6.day6 [] `shouldBe` "hello world"
-    describe "Day7b" $ do it "works" $ do Day6.day6b [] `shouldBe` "hello world"
     describe "Day8" $ do it "works" $ do Day6.day6 [] `shouldBe` "hello world"
     describe "Day8b" $ do it "works" $ do Day6.day6b [] `shouldBe` "hello world"
     describe "Day9" $ do it "works" $ do Day6.day6 [] `shouldBe` "hello world"
